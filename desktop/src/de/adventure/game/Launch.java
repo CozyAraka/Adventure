db714066815986502c0aae2f57da817a88bc14ff
Cozy <zaplek@protonmail.com>
2023-01-10 20:00:18 +0100

    neues icon - fullscreen black app

--- desktop/src/de/adventure/game/Launch.java
+++ desktop/src/de/adventure/game/Launch.java
@@ -2,15 +2,15 @@ package de.adventure.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import de.adventure.game.Game;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
-public class DesktopLauncher {
+public class Launch {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(200);
+		config.setForegroundFPS(240);
 		config.setTitle("Adventure");
 		config.setWindowIcon("icon.png");
+		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 		new Lwjgl3Application(new Game(), config);
 	}
 }
